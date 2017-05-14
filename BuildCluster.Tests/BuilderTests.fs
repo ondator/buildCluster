6f389c0e3909a8492d10e1a75c7d2dfb591742ce
vitaly derbin <vuderbin@gmail.com>
2017-05-14 19:06:14 +0300

    init

---
+++ BuildCluster.Tests/BuilderTests.fs
@@ -0,0 +1,25 @@
+﻿namespace BuildCluster.Tests
+
+module BuilderTests =
+    open NUnit.Framework
+    open BuildCluster
+
+    [<Test>]
+    let ``Build When Null Input Then Error``() = 
+        let sut = new Builder() :> IBuilder        
+        Assert.Throws<System.ArgumentNullException> 
+            (fun () -> sut.Build null |> ignore) 
+            |> ignore;
+
+    [<Test>]
+    let ``Build When Empty Input Then Error``() = 
+        let sut = new Builder() :> IBuilder        
+        Assert.Throws<System.ArgumentNullException> 
+            (fun () -> sut.Build System.String.Empty |> ignore) 
+            |> ignore;
+
+
+    
+
+
+
---
+++ BuildCluster/IBuilder.fs
@@ -0,0 +1,14 @@
+﻿namespace BuildCluster
+
+open System.Threading.Tasks
+
+type IBuilder = 
+    abstract Build : string -> unit
+
+type Builder() = 
+
+    interface IBuilder with
+        member this.Build text = 
+            match text with
+            | t when System.String.IsNullOrEmpty t -> nullArg "text"
+            | _ -> ()
