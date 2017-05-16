a046a148754fb4f6574daa3ecf40ba8da6dbbc98
vitaly derbin <vuderbin@gmail.com>
2017-05-16 23:33:04 +0300

    # compiling (part 1) # tests

--- BuildCluster.Tests/BuilderTests.fs
+++ BuildCluster.Tests/BuilderTests.fs
@@ -6,17 +6,57 @@ module BuilderTests =
 
     [<Test>]
     let ``Build When Null Input Then Error``() = 
-        let sut = new Builder() :> IBuilder        
-        Assert.Throws<System.ArgumentNullException> 
-            (fun () -> sut.Build null |> ignore) 
-            |> ignore;
+        let builder = new Builder() :> IBuilder
+
+        let sut = builder.Build null
+
+        Assert.AreEqual((Error [|"can't compile nothing"|]), sut);
 
     [<Test>]
     let ``Build When Empty Input Then Error``() = 
-        let sut = new Builder() :> IBuilder        
-        Assert.Throws<System.ArgumentNullException> 
-            (fun () -> sut.Build System.String.Empty |> ignore) 
-            |> ignore;
+        let builder = new Builder() :> IBuilder      
+        
+        let sut = builder.Build System.String.Empty
+
+        Assert.AreEqual((Error [|"can't compile nothing"|]), sut);
+    
+    [<Test>]
+    let ``Build When Do Nothing Valid C# Program Then Success Result``() = 
+        let doNothingProgram = @"using System;
+                                    namespace A
+                                    {
+                                        public class DoNothing
+                                        {
+                                            public void Do()
+                                            {
+                                            }
+                                        }
+                                    }"
+        
+        let builder = new Builder() :> IBuilder      
+        
+        let sut = builder.Build doNothingProgram
+
+        Assert.AreEqual(Success, sut)
+
+    [<Test>]
+    let ``Build When Do Nothing InValid C# Program Then Error``() = 
+        let doNothingProgram = @"using System;
+                                    namespace A
+                                    {
+                                        public cl ass DoNothing
+                                        {
+                                            public void Do()
+                                            {
+                                            }
+                                        }
+                                    }"
+        
+        let builder = new Builder() :> IBuilder      
+        
+        let sut = builder.Build doNothingProgram
+
+        Assert.AreEqual(Error [|"can't compile nothing"|], sut)
 
 
     
--- BuildCluster/IBuilder.fs
+++ BuildCluster/IBuilder.fs
@@ -1,14 +1,44 @@
 ﻿namespace BuildCluster
 
 open System.Threading.Tasks
+open Microsoft.CodeAnalysis.CSharp
+open System.IO
+open System.Linq
+open Microsoft.CodeAnalysis.Emit
+open Microsoft.CodeAnalysis
+open System.Collections.Immutable
+
+type BuildResult = Success | Error of string[]
 
 type IBuilder = 
-    abstract Build : string -> unit
+    abstract Build : string -> BuildResult
 
 type Builder() = 
+    
+    let ReturnError(errors: ImmutableArray<Diagnostic>) : BuildResult  = 
+        match errors.Any() with
+            | false -> Error [|"unknown error"|]
+            | _ -> Error (errors.Where(fun d -> 
+            d.IsWarningAsError || 
+            d.Severity = DiagnosticSeverity.Error).Select(fun d -> d.GetMessage()).ToArray())
+
+    let RunTests(emitResult: EmitResult) : BuildResult  = 
+        Success
+
+    let ProcessResult(emitResult: EmitResult) : BuildResult = 
+        match emitResult.Success with
+            | false -> ReturnError emitResult.Diagnostics
+            | _ -> RunTests emitResult
+
+    let Compile(text:string):BuildResult = 
+        let syntaxTree = CSharpSyntaxTree.ParseText text;
+        let compilation = CSharpCompilation.Create (Path.GetRandomFileName(), [|syntaxTree|])
+        use stream = new MemoryStream()
+        let emitResult = compilation.Emit stream
+        ProcessResult emitResult
 
     interface IBuilder with
         member this.Build text = 
-            match text with
-            | t when System.String.IsNullOrEmpty t -> nullArg "text"
-            | _ -> ()
+            match System.String.IsNullOrEmpty text with
+            | true -> Error [|"can't compile nothing"|]
+            | _ -> Compile text
